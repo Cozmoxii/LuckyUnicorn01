6d21a758ada3f2169a0966b11b906d3e6321cff9
Cozmoxii <79941979+Cozmoxii@users.noreply.github.com>
2021-03-12 12:29:29 +1300

    Works with expected data

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace LuckyUnicorn01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Lucky Unicorn!!\n\n" +
+                "Have you played before? y/n");
+            char userinput = Convert.ToChar(Console.ReadLine().ToLower()[0]);
+
+            if (userinput == 'y')
+            {
+                Console.WriteLine("Start game");
+            }
+
+            else if (userinput == 'n')
+            {
+                Console.WriteLine("Show Instructions");
+            }
+            else
+            {
+                Console.WriteLine("----ERROR PLEASE ENTER Y/N----");
+            }
         }
     }
 }
