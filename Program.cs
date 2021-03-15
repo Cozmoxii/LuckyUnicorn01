a63d4ddd0c02fbfa7442646e793bd80263493c2d
Sinead Galloway <singall@chbc.school.nz>
2021-03-16 09:32:17 +1300

    CheckYesNo is now a method so the question loops if the user does not enter yes or no.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,58 @@
-﻿using System;
+﻿//imports
+using System;
 
 namespace LuckyUnicorn01
 {
     class Program
     {
-        static void Main(string[] args)
+        //global variables
+
+        //methods
+
+        private static string CheckYesNo()
         {
+           
 
-            Console.WriteLine("Welcome to Lucky Unicorn!!\n\n");
-
-            bool flag = true;
-
-            while (flag)
+            while (true)
             {
                 Console.WriteLine("Have you played before? y/n");
                 char userinput = Convert.ToChar(Console.ReadLine().ToLower()[0]);
 
                 if (userinput == 'y')
                 {
-                    Console.WriteLine("Start game");
-                    flag = false;
+                    return "y";                  
                 }
 
                 else if (userinput == 'n')
                 {
-                    Console.WriteLine("Show Instructions");
-                    flag = false;
+                    return "n";
                 }
                 else
                 {
                     Console.WriteLine("----ERROR PLEASE ENTER Y/N----");
                 }
             }
+
+            //return "";
+        }
+
+        //main process
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Lucky Unicorn!!\n\n");
+
+            if (CheckYesNo().Equals("y")) 
+            {
+                Console.WriteLine("start game");
+            }
+            else
+            {
+                Console.WriteLine("Show Instructions");
+                Console.WriteLine("start game");
+            }
+
+            
            
         }
     }
